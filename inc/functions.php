b5a7641147b3de64a9dc90b5cd24dcefefec25ea
Stav Kidron <kidronstav@gmail.com>
2021-01-26 02:08:40 -0600

    added link to full catalog

--- inc/functions.php
+++ inc/functions.php
@@ -15,15 +15,21 @@
 
     function array_category($catalog, $category){
 
-        if($category == null){
-            return array_keys($catalog);
-        }
+        // if($category == null){
+        //     return array_keys($catalog);
+        // }
         $output = array();
         foreach($catalog as $id => $item) {
-            if(strtolower($category) == strtolower($item["category"])) {
-                $output[] = $id;
+            if($category == null OR strtolower($category) == strtolower($item["category"])) {
+                $sort = $item["title"];
+                $sort = ltrim($sort, "The ");
+                $sort = ltrim($sort, "A ");
+                $sort = ltrim($sort, "An ");
+                $output[$id] = $sort;
             }
         }
 
-        return $output;
+        asort($output);
+
+        return array_keys($output);
     }
