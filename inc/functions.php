73f8c39aa902bffa5b60b52f2e5fb7037796144f
Stav Kidron <kidronstav@gmail.com>
2021-01-26 01:56:21 -0600

    all media displaying in full catalog

--- inc/functions.php
+++ inc/functions.php
@@ -12,3 +12,18 @@
 
         return $output;
     }
+
+    function array_category($catalog, $category){
+
+        if($category == null){
+            return array_keys($catalog);
+        }
+        $output = array();
+        foreach($catalog as $id => $item) {
+            if(strtolower($category) == strtolower($item["category"])) {
+                $output[] = $id;
+            }
+        }
+
+        return $output;
+    }
